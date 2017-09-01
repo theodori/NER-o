90ccbd34d796332be0baa337019bcf31bf21f341
evangelos <evangelos.theodoridis@springernature.com>
2017-09-01 16:42:49 +0100

    Evangelos: create RDF for annotations

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -2,9 +2,11 @@ import modules.GeneralNLTKNerModuleImpl
 
 object Test {
   def main(args: Array[String]): Unit = {
+    val input="Athens is a global city and one of the biggest economic " +
+      "centres in southeastern Europe. the United Nations are hosted there. It has a large financial sector, and its port Piraeus is both the largest passenger port in Europe"
     val module = new GeneralNLTKNerModuleImpl();
-    val result: Some[String] = module.process("Athens is a global city and one of the biggest economic " +
-      "centres in southeastern Europe. the United Nations are hosted there. It has a large financial sector, and its port Piraeus is both the largest passenger port in Europe")
+    val result: Some[String] = module.process(input)
     println(result.get)
+    println(module.createRDF(input))
   }
 }
--- src/main/scala/modules/GeneralNLTKNerModuleImpl.scala
+++ src/main/scala/modules/GeneralNLTKNerModuleImpl.scala
@@ -1,6 +1,9 @@
 package modules
 
+import java.util
+
 import ner.modules.NerModule
+import scala.collection.JavaConverters._
 
 // it is required to have installed python
 class GeneralNLTKNerModuleImpl() extends NerModule {
@@ -16,6 +19,35 @@ class GeneralNLTKNerModuleImpl() extends NerModule {
         throw new RuntimeException(result._3)
     }
 
+  }
+
+  def createRDF(input: String): String = {
+    import org.nlp2rdf.bean.{NIFBean, NIFType}
+    import org.nlp2rdf.nif21.impl.NIF21
+    val contextBuilder = new NIFBean.NIFBeanBuilder
+    contextBuilder.context("http://test/resource").mention(input).nifType(NIFType.CONTEXT)
+    val beanContext = new NIFBean(contextBuilder)
+
+    val types: util.List[String] = Seq(
+      "http://dbpedia.org/ontology/Person",
+      "http://dbpedia.org/ontology/Place",
+      "http://dbpedia.org/ontology/Organization"
+
+    ).asJava
+
+
+    val entityBuilder = new NIFBean.NIFBeanBuilder
+    entityBuilder.context("http://test/annotation").mention("Argentina").beginIndex(23).endIndex(32)
+      .annotator("http://www.nltk.org/ne_chunk")
+      .types(types);
+
+
+    val beans = Seq(
+      new NIFBean(entityBuilder),
+      new NIFBean(contextBuilder)
+    ).asJava
+    val nif = new NIF21(beans)
+    nif.getTurtle
   }
 
 
